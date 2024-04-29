0c2c323fcb0339638ba766854aa4fc7c07a6cfba
Mark <fqchenxin@gmail.com>
2024-04-29 20:39:49 +1000

    add tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Grid1 from './pages/Grid1.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:  <App />,
+  },
+  {
+    path: "/grid1",
+    element:  <Grid1 />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ src/pages/Grid1.tsx
@@ -0,0 +1,9 @@
+const Grid1 = () => {
+  return (
+    <div className="bg-[red]">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+};
+
+export default Grid1;
